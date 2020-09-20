0828eb9c3af25ba6c7017c908e42405fe05da205
RemainingToast <remainingtoast@gmail.com>
2020-09-20 23:29:28 +1000

    Add a bunch of functionality to the bot

--- bot.js
+++ bot.js
@@ -1,10 +1,47 @@
-let test = 5
-console.log(test)
 const mineflayer = require('mineflayer')
-
-const details = {
-    host: 'localhost',
-    port: 25565
+if (process.argv.length < 4 || process.argv.length > 6) {
+    console.log('Usage : node bot.js <host> <port> [<name>] [<password>]')
+    process.exit(1)
 }
+function create(){
+    console.log("Bot is booting...")
+    const bot = mineflayer.createBot({
+        host: process.argv[2],
+        port: parseInt(process.argv[3]),
+        username: process.argv[4] ? process.argv[4] : 'ToastmcB0T',
+        password: process.argv[5]
+    })
+    const msgDelay = 5
+    const logDelay = 5
 
-const bot = mineflayer.createBot(details)
+    bot.on('login', () => {
+        console.log("[Bot] I have logged in successfully!")
+        const mcData = require('minecraft-data')(bot.version) // You know the version when the bot has spawned
+        const totemId = mcData.itemsByName.totem_of_undying.id // Get the correct id
+        if (mcData.itemsByName.totem_of_undying) {
+            setInterval(() => {
+                const totem = bot.inventory.findInventoryItem(totemId, null, false);
+                if (totem) {
+                    bot.equip(totem, 'off-hand')
+                }
+            }, 50)
+        }
+    })
+    bot.on('message', (message) => {
+        console.log("[Chat] " + message.toString())
+    })
+    bot.on('whisper', (username, message, rawMessage) => {
+        console.log(`[Bot] I received a message from ${username}: ${message}`)
+        bot.whisper(username, 'No u')
+    })
+    bot.on('playerJoined', (player) => {
+        if (player.username !== bot.username) {
+            bot.whisper(player.username, "Toastmc.dev on TOP")
+            console.log(`[Bot] I sent ${player.username} a message: Toastmc.dev on TOP`)
+        }
+    })
+    bot.on('kicked', (reason, loggedIn) => console.log("[Bot] " + reason, loggedIn))
+    bot.on('error', (err) => console.log(err))
+    bot.on('end', create)
+}
+create()
