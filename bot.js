fee6dab2d23a938886831e76096d683b48485ad2
RemainingToast <remainingtoast@gmail.com>
2020-09-20 21:45:08 +1000

    Init

---
+++ bot.js
@@ -0,0 +1,10 @@
+let test = 5
+console.log(test)
+const mineflayer = require('mineflayer')
+
+const details = {
+    host: 'localhost',
+    port: 25565
+}
+
+const bot = mineflayer.createBot(details)
